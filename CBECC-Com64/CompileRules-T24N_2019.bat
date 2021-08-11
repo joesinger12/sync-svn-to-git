30e7f68ae496bee3f14615dbb9c0ea4bd686ee28
Scott Criswell <scott.a.criswell@gmail.com>
2021-08-11 18:15:03 +0000

    SVN Revision #6659 by scriswellwsf       CBECC-Com 2019/22 (1201) executables w/ updates enabling access to new MFam zone &amp; envelope objects (shared w/ Res rules) in component tree

--- CBECC-Com64/CompileRules-T24N_2019.bat
+++ CBECC-Com64/CompileRules-T24N_2019.bat
@@ -1,6 +1,6 @@
 if not exist "Data\Rulesets" mkdir "Data\Rulesets"
 if not exist "Data\Rulesets\T24N_2019" mkdir "Data\Rulesets\T24N_2019"
-if exist BEMCompiler19c.exe BEMCompiler19c.exe --bemBaseTxt="../RulesetDev/Rulesets/BEMBase.txt" --bemEnumsTxt="../RulesetDev/Rulesets/T24N/T24N BEMEnums.txt" --bemBaseBin="Data/Rulesets/T24N_2019/T24N_2019 BEMBase.bin" --rulesTxt="../RulesetDev/Rulesets/T24N/Rules/T24N_2019.txt" --rulesBin="Data/Rulesets/T24N_2019.bin" --rulesLog="_T24N-2019 Rules Log.out" --compileDM --compileRules
+if exist BEMCompiler19c.exe BEMCompiler19c.exe --sharedPath1="../RulesetDev/Rulesets/shared/" --bemBaseTxt="../RulesetDev/Rulesets/BEMBase.txt" --bemEnumsTxt="../RulesetDev/Rulesets/T24N/T24N BEMEnums.txt" --bemBaseBin="Data/Rulesets/T24N_2019/T24N_2019 BEMBase.bin" --rulesTxt="../RulesetDev/Rulesets/T24N/Rules/T24N_2019.txt" --rulesBin="Data/Rulesets/T24N_2019.bin" --rulesLog="_T24N-2019 Rules Log.out" --compileDM --compileRules
 echo OFF
 echo BEMCompiler19c.exe returned (%ERRORLEVEL%) for T24N_2019
 if %ERRORLEVEL%==0 goto :copyfiles
---
+++ CBECC-Com64/CopyScreens_T24N_2022.bat
@@ -0,0 +1,4 @@
+copy "..\RulesetDev\Rulesets\T24N\T24N_2022 Screens.txt"  "..\CBECC-Com64\Data\Rulesets\T24N_2022\T24N_2022 Screens.txt"
+copy "..\RulesetDev\Rulesets\T24N\T24N ToolTips.txt" "..\CBECC-Com64\Data\Rulesets\T24N_2022\T24N_2022 ToolTips.txt"
+copy "..\RulesetDev\Rulesets\T24N\*.jpg" "..\CBECC-Com64\Data\Rulesets\T24N_2022\*.*"
+copy "..\RulesetDev\Rulesets\T24N\RTF\*.*" "..\CBECC-Com64\Data\Rulesets\T24N_2022\RTF\*.*"
