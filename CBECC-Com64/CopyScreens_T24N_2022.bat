a88df36fe07dc59d80423ec0ce8b4f7387bd3113
Joe Singer <123663997+joesinger12@users.noreply.github.com>
2024-09-19 12:14:05 -0400

    starting at rev 6651 (start of MF branch)

--- CBECC-Com64/CopyScreens_T24N_2022.bat
+++
@@ -1,6 +0,0 @@
-copy "..\RulesetDev\Rulesets\T24N\T24N_2022 Screens.txt"  "..\CBECC-Com64\Data\Rulesets\T24N_2022\T24N_2022 Screens.txt"
-copy "..\RulesetDev\Rulesets\T24N\T24N ToolTips.txt" "..\CBECC-Com64\Data\Rulesets\T24N_2022\T24N_2022 ToolTips.txt"
-copy "..\RulesetDev\Rulesets\T24N\*.jpg" "..\CBECC-Com64\Data\Rulesets\T24N_2022\*.*"
-copy "..\RulesetDev\Rulesets\T24N\RTF\*.*" "..\CBECC-Com64\Data\Rulesets\T24N_2022\RTF\*.*"
-copy "..\RulesetDev\Rulesets\shared\Screens*.txt" "..\CBECC-Com64\Data\Rulesets\T24N_2022\*.*"
-copy "..\RulesetDev\Rulesets\shared\*.jpg" "..\CBECC-Com64\Data\Rulesets\T24N_2022\*.*"
